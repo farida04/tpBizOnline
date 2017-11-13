046b2453d7e24a1ec56e1c1541d8e1fbebc52417
farida04 <f.oumatous@gmail.com>
2017-11-13 20:10:39 +0100

    petit changement en js et html

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,10 @@ const bizOnline = (function (){
   var tabl = [];
 
   var Produit = function (n, p, d, r){
-    this.nom= n;
-    this.prix= p;
-    this.description= d;
-    this.ref= r;
+    this.nom= n,
+    this.prix= p,
+    this.description= d,
+    this.ref= r
 
   };
 
@@ -15,13 +15,11 @@ const bizOnline = (function (){
     let tablLettre = ["A", "B", "C", "D", "E"];
     let choixLettre = Math.floor(Math.random()*5);
     let ref = tablLettre[choixLettre];
-    let ajout = document.getElementById('resultRef');
 
     for (var i = 0; i < 5; i++) {
       chiffreAleatoire = Math.floor(Math.random()*9);
       ref += chiffreAleatoire;
     };
-    ajout.innerHTML = ref;
     return ref;
   };
 
@@ -29,31 +27,32 @@ const bizOnline = (function (){
     var nom = document.getElementById('nom').value;
     var prix = document.getElementById('prix').value;
     var description = document.getElementById('description').value;
-    var btnRef = document.getElementById('ref');
-    console.log(description);
     var ref = refAleatoire();
-    btnRef.onclick = refAleatoire;
-    console.log(ref);
     tabl.push(new Produit(nom, prix, description, ref));
     console.log(tabl);
+    ajouter();
 
     return tabl;
   };
 
   const ajouter = function(){
     var caseTableau = document.getElementById('tbody');
-    caseTableau.innerHTML = <tr>
-
+    var temp = "";
+    console.log(caseTableau);
+    for (var i = 0; i < tabl.length; i++) {
+      temp += "<tr><td>"+ tabl[i].nom + "</td>"+
+      "<td>"+ tabl[i].prix + "</td>"+
+      "<td>"+ tabl[i].description +"</td>"+
+      "<td>"+tabl[i].ref +"</td></tr>";
+    };
+    caseTableau.innerHTML = temp;
   };
 
   window.onload = function init(){
+
     var valider = document.getElementById('valider');
-    // valider.
-
-
-
-    // btnRef.onclick = refAleatoire;
     valider.onclick = creerProduit;
+
 
 
 
